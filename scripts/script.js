37c3138069f222250b69137d60e119928a7afb4f
TrezorTop <trezorsix@mail.ru>
2020-10-15 13:44:49 +0300

    added slider to "my-works"

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
-$(document).ready(() => {
-    
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 3,
+        margin:30,
+        nav: true,
+        // responsive: {
+        //     0: {
+        //         items: 1
+        //     },
+        //     600: {
+        //         items: 3
+        //     },
+        //     1000: {
+        //         items: 5
+        //     }
+        // }
+    })
+
 });
