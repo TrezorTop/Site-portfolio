36f0c08f7759a549e2a854399881b60f0b4a6b52
TrezorTop <trezortop92@gmail.com>
2020-10-17 13:06:34 +0300

    немного фиксов, добавлена адаптивность под 1024

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 $(document).ready(function () {
 
-    $('.owl-carousel').owlCarousel({
+    var owl = $('.owl-carousel');
+
+    owl.owlCarousel({
         loop: true,
         items: 3,
         margin: 30,
         nav: true,
-    })
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1025: {
+                items: 3
+            }
+        }
+    });
 
 });
