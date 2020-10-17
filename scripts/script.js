7da526729f4a5b1d51667270c28a45d8ebd90305
TrezorTop <trezortop92@gmail.com>
2020-10-17 17:04:53 +0300

    Адапативность меню, зафиксированный верхний блок

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,38 @@
 $(document).ready(function () {
 
-    var owl = $('.owl-carousel');
+    OwlCarousel();
 
-    owl.owlCarousel({
-        loop: true,
-        items: 3,
-        margin: 30,
-        nav: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            700: {
-                items: 2
-            },
-            1025: {
-                items: 3
-            }
+    $('#menu-btn').click(menuController);
+
+    function menuController() {
+        if ($('#menu-btn').hasClass('active')) {
+            $('.navbar-left').slideToggle(400);
+        } else {
+            $('.navbar-left').slideToggle(400);
         }
-    });
+
+        $('#menu-btn').toggleClass('active');
+    }
+
+    function OwlCarousel() {
+        var owl = $('.owl-carousel');
+
+        owl.owlCarousel({
+            loop: true,
+            items: 3,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                700: {
+                    items: 2
+                },
+                1025: {
+                    items: 3
+                }
+            }
+        });
+    }
 
 });
